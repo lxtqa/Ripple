a026952c6dba4ec320a4983135a02d52cdeb9106
Zhen Tang <silenceofdaybreak@gmail.com>
2020-09-27 11:17:56 +0800

    ripple-agent: Add HybridLogicalClock from nacos.

---
+++ agent/src/main/java/ripple/agent/core/hlc/resolver/AddWinConflictResolver.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright 1999-2019 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ripple.agent.core.hlc.resolver;
+
+import ripple.agent.core.hlc.*;
+
+import java.util.List;
+
+/**
+ * An Add-win resolver which guarantee the "Add win" semantics.
+ * Currently, it solves conflicts by calculating union of two sets, and discards removal operations.
+ * Still needs to be improved for a production-ready CRDT (Conflict-free Replicated Data Type).
+ *
+ * @author lostcharlie
+ */
+public class AddWinConflictResolver implements OperationBasedConflictResolver {
+    private long maxTimeDifference;
+
+    public long getMaxTimeDifference() {
+        return maxTimeDifference;
+    }
+
+    private void setMaxTimeDifference(long maxTimeDifference) {
+        this.maxTimeDifference = maxTimeDifference;
+    }
+
+    public AddWinConflictResolver(long maxTimeDifference) {
+        this.setMaxTimeDifference(maxTimeDifference);
+    }
+
+    @Override
+    public void merge(Datum current, Operation toApply) {
+        if (!(current.value instanceof Instances)) {
+            // Only merge instances now
+            return;
+        }
+        long timeDifference = toApply.getTimestamp().get() - current.timestamp.get();
+        if (timeDifference < 0 && (-timeDifference) > this.getMaxTimeDifference()) {
+            // Received obsoleted data, discard
+            return;
+        }
+        if (timeDifference > 0 && timeDifference > this.getMaxTimeDifference()) {
+            // Accept
+            Instances instances = (Instances) current.value;
+            if (toApply.getOperationType() == OperationType.ADD_INSTANCE) {
+                this.doAddInstances(toApply, instances);
+            } else if (toApply.getOperationType() == OperationType.REMOVE_INSTANCE) {
+                List<Instance> toRemoveList = ((Instances) toApply.getTargetValue()).getInstanceList();
+                for (Instance toRemove : toRemoveList) {
+                    instances.getInstanceList().remove(toRemove);
+                }
+            }
+        } else {
+            // Resolve conflict
+            Instances instances = (Instances) current.value;
+            if (toApply.getOperationType() == OperationType.ADD_INSTANCE) {
+                this.doAddInstances(toApply, instances);
+            }
+            // Discard conflict removal operation
+        }
+        if (timeDifference > 0) {
+            current.timestamp.set(toApply.getTimestamp().get());
+        }
+    }
+
+    private void doAddInstances(Operation toApply, Instances instances) {
+        List<Instance> toAddList = ((Instances) toApply.getTargetValue()).getInstanceList();
+        for (Instance toAdd : toAddList) {
+            if (!(instances.getInstanceList().contains(toAdd))) {
+                instances.getInstanceList().add(toAdd);
+            }
+        }
+    }
+}
---
+++ agent/src/main/java/ripple/agent/core/hlc/resolver/OperationBasedConflictResolver.java
@@ -0,0 +1,19 @@
+package ripple.agent.core.hlc.resolver;
+
+import ripple.agent.core.hlc.Datum;
+import ripple.agent.core.hlc.Operation;
+
+/**
+ * Public interface for a operation based conflict resolver.
+ *
+ * @author lostcharlie
+ */
+public interface OperationBasedConflictResolver {
+    /**
+     * Apply target operation on local data replica and resolve conflicts for concurrent operations
+     *
+     * @param current current value of the datum
+     * @param toApply the operation to apply
+     */
+    void merge(Datum current, Operation toApply);
+}
