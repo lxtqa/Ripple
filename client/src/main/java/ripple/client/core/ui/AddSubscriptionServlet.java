6d5a9cd8315b90a296bc0cb87dcdb2f91fe3505f
Zhen Tang <silenceofdaybreak@gmail.com>
2020-12-02 16:02:45 +0800

    ripple-client: UI for AddSubscription.

--- client/src/main/java/ripple/client/core/ui/AddSubscriptionServlet.java
+++ client/src/main/java/ripple/client/core/ui/AddSubscriptionServlet.java
@@ -5,7 +5,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ripple.client.RippleClient;
 import ripple.client.core.BaseServlet;
+import ripple.client.core.Endpoint;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -23,12 +25,48 @@ public class AddSubscriptionServlet extends BaseServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         LOGGER.info("[AddSubscriptionServlet] Receive GET request.");
+        StringBuilder stringBuilder = new StringBuilder();
 
-        // TODO
+        stringBuilder.append("                <form action=\"").append(Endpoint.UI_ADD_SUBSCRIPTION).append("\" method=\"post\">\n");
+        stringBuilder.append("                    <div class=\"form-group\">\n");
+        stringBuilder.append("                        <label for=\"applicationName\">应用名称</label>\n");
+        stringBuilder.append("                        <input id=\"applicationName\" name=\"applicationName\" class=\"form-control\" type=\"text\" />\n");
+        stringBuilder.append("                    </div>\n");
+        stringBuilder.append("                    <div class=\"form-group\">\n");
+        stringBuilder.append("                        <label for=\"key\">键</label>\n");
+        stringBuilder.append("                        <input id=\"key\" name=\"key\" class=\"form-control\" type=\"text\" />\n");
+        stringBuilder.append("                    </div>\n");
+        stringBuilder.append("                    <div class=\"form-group\">\n");
+        stringBuilder.append("                        <button type=\"submit\" class=\"form-control btn btn-outline-primary\">订阅新配置</button>\n");
+        stringBuilder.append("                    </div>\n");
+        stringBuilder.append("                </form>\n");
+        String content = stringBuilder.toString();
+
+        String pageContent = PageGenerator.buildPage("Ripple Client - 订阅新配置", "订阅新配置", content);
+
+        response.setContentType("text/html;charset=UTF-8");
+        response.setStatus(HttpStatus.OK_200);
+        response.getWriter().println(pageContent);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String applicationName = request.getParameter("applicationName");
+        String key = request.getParameter("key");
+        LOGGER.info("[AddSubscriptionServlet] Receive POST request. applicationName = {}, key = {}."
+                , applicationName, key);
+
+        boolean success = this.getClient().subscribe(applicationName, key);
 
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append("                <p>\n");
-        stringBuilder.append("                    ").append("订阅新配置").append("\n");
+        stringBuilder.append("                    ");
+        if (success) {
+            stringBuilder.append("操作已成功完成。");
+        } else {
+            stringBuilder.append("出现错误，请重试。");
+        }
+        stringBuilder.append("\n");
         stringBuilder.append("                </p>\n");
         String content = stringBuilder.toString();
 
