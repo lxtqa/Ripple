a9b966e5805a0d44960d0e3a4ac99aba84f942e4
Zhen Tang <silenceofdaybreak@gmail.com>
2023-05-18 09:56:24 +0800

    ripple: Add license.

--- ripple-client/src/main/java/ripple/client/core/tcp/handler/DeleteResponseHandler.java
+++ ripple-client/src/main/java/ripple/client/core/tcp/handler/DeleteResponseHandler.java
@@ -1,9 +1,12 @@
-// ===============================================================
-// Copyright(c) Institute of Software, Chinese Academy of Sciences
-// Copyright(c) Nanjing Institute of Software Technology
-// ===============================================================
-// Author : Zhen Tang <tangzhen12@otcaix.iscas.ac.cn>
-// Date   : 2021-11-26
+// Copyright (c) 2023 Institute of Software, Chinese Academy of Sciences
+// Ripple is licensed under Mulan PSL v2.
+// You can use this software according to the terms and conditions of the Mulan PSL v2.
+// You may obtain a copy of Mulan PSL v2 at:
+//          http://license.coscl.org.cn/MulanPSL2
+// THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+// EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+// MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+// See the Mulan PSL v2 for more details.
 
 package ripple.client.core.tcp.handler;
 
