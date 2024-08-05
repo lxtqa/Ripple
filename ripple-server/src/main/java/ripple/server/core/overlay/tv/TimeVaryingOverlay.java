01d636aee83b825dbf55a1d1145047e758632e92
Zhen Tang <silenceofdaybreak@gmail.com>
2024-08-05 19:58:40 +0800

    Add stubs.
    Signed-off-by: Zhen Tang <silenceofdaybreak@gmail.com>

---
+++ ripple-server/src/main/java/ripple/server/core/overlay/tv/TimeVaryingOverlay.java
@@ -0,0 +1,39 @@
+// Copyright (c) 2024 Institute of Software, Chinese Academy of Sciences
+// Ripple is licensed under Mulan PSL v2.
+// You can use this software according to the terms and conditions of the Mulan PSL v2.
+// You may obtain a copy of Mulan PSL v2 at:
+//          http://license.coscl.org.cn/MulanPSL2
+// THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+// EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+// MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+// See the Mulan PSL v2 for more details.
+
+package ripple.server.core.overlay.tv;
+
+import ripple.common.entity.AbstractMessage;
+import ripple.common.entity.NodeMetadata;
+import ripple.server.core.overlay.Overlay;
+
+import java.util.List;
+
+/**
+ * @author Zhen Tang
+ */
+public class TimeVaryingOverlay implements Overlay {
+    @Override
+    public void buildOverlay(List<NodeMetadata> nodeList) {
+        // TODO
+    }
+
+    @Override
+    public List<NodeMetadata> calculateNodesToSync(AbstractMessage message, NodeMetadata source, NodeMetadata current) {
+        // TODO
+        return null;
+    }
+
+    @Override
+    public List<NodeMetadata> calculateNodesToCollectAck(AbstractMessage message) {
+        // TODO
+        return null;
+    }
+}
