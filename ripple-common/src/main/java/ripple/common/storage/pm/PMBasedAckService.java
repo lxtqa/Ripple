7d05f7d7f4ed7c9e22cba6220c793474ee3a987a
Zhen Tang <silenceofdaybreak@gmail.com>
2024-08-22 15:59:29 +0800

    ripple-common: Implement PMBasedAckService.
    Signed-off-by: Zhen Tang <silenceofdaybreak@gmail.com>

--- ripple-common/src/main/java/ripple/common/storage/pm/PMBasedAckService.java
+++ ripple-common/src/main/java/ripple/common/storage/pm/PMBasedAckService.java
@@ -10,12 +10,21 @@
 
 package ripple.common.storage.pm;
 
+import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import ripple.common.entity.Ack;
 import ripple.common.storage.AckService;
+import ripple.common.storage.StorageHelper;
 
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -26,6 +35,7 @@ public class PMBasedAckService implements AckService {
     private static final ObjectMapper MAPPER = new ObjectMapper();
     private PMBasedStorage storage;
     private ConcurrentHashMap<UUID, Object> locks;
+    private static final String KEY_ACK_LIST = "ack-list";
 
     public PMBasedStorage getStorage() {
         return storage;
@@ -55,27 +65,113 @@ public class PMBasedAckService implements AckService {
         return this.getLocks().get(messageUuid);
     }
 
+    private String getKeyForAck(UUID messageUuid) {
+        return "ack-" + messageUuid.toString();
+    }
+
+    private Set<UUID> getAckList() {
+        try {
+            JavaType listType = MAPPER.getTypeFactory().constructCollectionType(HashSet.class, UUID.class);
+            byte[] valueBytes = this.getStorage().getPmCacheAdapter()
+                    .get(PMBasedAckService.KEY_ACK_LIST.getBytes(StandardCharsets.UTF_8));
+            if (valueBytes != null) {
+                String value = new String(valueBytes, StandardCharsets.UTF_8);
+                return MAPPER.readValue(value, listType);
+            } else {
+                return new HashSet<>();
+            }
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return null;
+        }
+    }
+
+    private void writeAckList(Set<UUID> uuids) {
+        try {
+            String value = MAPPER.writeValueAsString(uuids);
+            this.getStorage().getPmCacheAdapter()
+                    .put(PMBasedAckService.KEY_ACK_LIST.getBytes(StandardCharsets.UTF_8)
+                            , value.getBytes(StandardCharsets.UTF_8));
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+
     @Override
     public boolean initAck(UUID messageUuid, List<Integer> nodeList) {
-        // TODO
-        return false;
+        synchronized (this.getLock(messageUuid)) {
+            try {
+                Set<UUID> ackList = this.getAckList();
+                ackList.add(messageUuid);
+                this.writeAckList(ackList);
+
+                Ack ack = new Ack();
+                ack.setMessageUuid(messageUuid);
+                ack.setNodeList(new HashSet<>(nodeList));
+                ack.setAckNodes(new HashSet<>());
+
+                String key = this.getKeyForAck(messageUuid);
+                String value = MAPPER.writeValueAsString(ack);
+                this.getStorage().getPmCacheAdapter().put(key.getBytes(StandardCharsets.UTF_8), value.getBytes(StandardCharsets.UTF_8));
+                return true;
+            } catch (Exception exception) {
+                exception.printStackTrace();
+                return false;
+            }
+        }
     }
 
     @Override
     public Ack getAck(UUID messageUuid) {
-        // TODO
-        return null;
+        synchronized (this.getLock(messageUuid)) {
+            try {
+                String key = this.getKeyForAck(messageUuid);
+                byte[] valueBytes = this.getStorage().getPmCacheAdapter().get(key.getBytes(StandardCharsets.UTF_8));
+                if (valueBytes == null) {
+                    return null;
+                }
+                String value = new String(valueBytes, StandardCharsets.UTF_8);
+                return MAPPER.readValue(value, Ack.class);
+            } catch (Exception exception) {
+                exception.printStackTrace();
+                return null;
+            }
+        }
     }
 
     @Override
     public List<Ack> getAllAcks() {
-        // TODO
-        return Collections.emptyList();
+        try {
+            Set<UUID> ackList = this.getAckList();
+            List<Ack> ret = new ArrayList<>();
+            if (ackList != null) {
+                for (UUID uuid : ackList) {
+                    ret.add(this.getAck(uuid));
+                }
+            }
+            return ret;
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return null;
+        }
     }
 
     @Override
     public boolean recordAck(UUID messageUuid, int serverId) {
-        // TODO
-        return false;
+        synchronized (this.getLock(messageUuid)) {
+            try {
+                Ack ack = this.getAck(messageUuid);
+                if (!ack.getAckNodes().contains(serverId)) {
+                    ack.getAckNodes().add(serverId);
+                }
+                String value = MAPPER.writeValueAsString(ack);
+                String key = this.getKeyForAck(messageUuid);
+                this.getStorage().getPmCacheAdapter().put(key.getBytes(StandardCharsets.UTF_8), value.getBytes(StandardCharsets.UTF_8));
+                return true;
+            } catch (Exception exception) {
+                exception.printStackTrace();
+                return false;
+            }
+        }
     }
 }
